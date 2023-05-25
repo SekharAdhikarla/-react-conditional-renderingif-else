697ecec1e1bbb2cc55e1c3a7968261ca420c026b
SekharAdhikarla <Adhikarlavenkatasekhar@gmail.com>
2023-05-25 05:13:13 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/button-has-type */
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {isLoggedIn: true}
+
+  renderAuthButton = () => {
+    const {isLoggedIn} = this.state
+    if (isLoggedIn === true) {
+      return <button>Logout</button>
+    }
+    return <button>Login</button>
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Welcome name="SEkhar" />
+        {this.renderAuthButton()}
+      </div>
+    )
+  }
+}
+export default App
